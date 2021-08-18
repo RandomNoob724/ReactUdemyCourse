2a86779ccdc029b6b1f5f686e39b07d82e3edd71
Emil Persson <emilper1999@gmail.com>
2021-08-18 14:09:44 +0200

    year filter added

--- src/components/Expenses/ExpenseFrame.js
+++ src/components/Expenses/ExpenseFrame.js
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import "./ExpenseFrame.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
-const ExpenseFrame = (props) =>{
+const ExpenseFrame = (props) => {
+  const [yearFilter, setYearFilter] = useState("2020");
+
+  const onYearFilterChangeHandler = (year) => {
+    setYearFilter(year);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          date={expense.date}
-          title={expense.title}
-          amount={expense.amount}
-        />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter yearValue={yearFilter} onYearFilterChange={onYearFilterChangeHandler} />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            amount={expense.amount}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 
 export default ExpenseFrame;
